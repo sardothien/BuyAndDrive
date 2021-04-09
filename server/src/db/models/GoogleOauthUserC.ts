5ba75b28cf9deefb8552a29646604d0f94b30c95
Luka Vujcic <lukavujcic@hotmail.com>
2021-04-09 12:50:30 +0200

    Remove createdAt and updateAt from GoogleOauthUser table

--- server/src/db/models/GoogleOauthUserC.ts
+++ server/src/db/models/GoogleOauthUserC.ts
@@ -30,16 +30,6 @@ export const GoogleOauthUserC = (sequelize: Sequelize): GoogleOauthUserStatic =>
         type: DataTypes.STRING,
         allowNull: false
       },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
     }, {
       freezeTableName: true,
     }
