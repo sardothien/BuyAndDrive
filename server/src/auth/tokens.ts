c7cc21a89cac5c0cd8ed574f5fd99de17cb5a7f8
imafikus <aleksatesicteske@gmail.com>
2021-04-14 18:23:10 +0200

    Add JWT logic

---
+++ server/src/auth/tokens.ts
@@ -0,0 +1,80 @@
+import * as jwt from 'jsonwebtoken'
+import { envVal } from '../envVal';
+
+const generateToken = (userId: string, secretKey: string|undefined, expiration: string|undefined): string => {
+  if(!secretKey) {
+    throw new Error('No secret provided');
+  }
+  
+  if(!expiration) {
+    throw new Error('No expiration provided');
+  }
+  
+  return jwt.sign(
+    {
+      userId
+    },
+    secretKey,
+    {
+      expiresIn: expiration + 'm',
+    }
+  );
+}
+
+export const generateAccessToken = (userId: string): string => {
+
+  return generateToken(
+    userId,
+    envVal.jwtSecretKey,
+    envVal.jwtExpirationInMins,
+  );
+}
+
+export const generateRegistrationToken = (userId: string): string => {
+
+  return generateToken(
+    userId,
+    envVal.jwtSignUpExpirationInMins,
+    envVal.jwtSignUpSecretKey,
+  );
+}
+const verifyToken = (token: string | undefined, secretKey: string|undefined, expiration: string|undefined): string|undefined => {
+  
+  if(!token) {
+    return undefined;
+  }
+  
+  if(!secretKey) {
+    throw new Error('No secret key provided');
+  }
+  
+  if(!expiration) {
+    throw new Error('No expiration provided');
+  }
+
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, secretKey) as Record<string, string>;
+  } catch(err) {
+    return undefined;
+  }
+
+  return decodedToken.userId;
+}
+
+export const verifyAccessToken = (token: string|undefined): string|undefined => {
+  return verifyToken(
+    token,
+    envVal.jwtSecretKey,
+    envVal.jwtExpirationInMins
+  );
+}
+
+export const verifySignUpToken = (token: string|undefined): string|undefined => {
+  return verifyToken(
+    token,
+    envVal.jwtSignUpExpirationInMins,
+    envVal.jwtSignUpSecretKey
+  );
+}
--- server/src/envVal.ts
+++ server/src/envVal.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { string } from 'joi';
 
 dotenv.config();
 
@@ -7,21 +6,36 @@ export interface EnvValType {
   pgConnectionString: string;
   gmailUser: string,
   gmailPass: string,
+  jwtSecretKey: string;
+  jwtExpirationInMins: string;
+  jwtSignUpSecretKey: string;
+  jwtSignUpExpirationInMins: string;
 }
 
 const loadEnvVals = (): EnvValType => {
   if(
     !process.env.PGCONNECTION_STRING || 
     !process.env.GMAIL_USER || 
-    !process.env.GMAIL_PASS
+    !process.env.GMAIL_PASS ||
+    !process.env.JWT_SECRET_KEY || 
+    !process.env.JWT_EXPIRATION_IN_MINS || 
+    !process.env.JWT_SIGNUP_SECRET_KEY ||
+    !process.env.JWT_SIGNUP_EXPIRATION_IN_MINS
     ) {
       throw new Error('Invalid .env file, some values are undefined, please check .env.example to see what you are missing');
   }
 
   return {
     pgConnectionString: process.env.PGCONNECTION_STRING,
+    
     gmailUser: process.env.GMAIL_USER,
     gmailPass: process.env.GMAIL_PASS,
+    
+    jwtSecretKey: process.env.JWT_SECRET_KEY,
+    jwtExpirationInMins: process.env.JWT_EXPIRATION_IN_MINS,
+    
+    jwtSignUpSecretKey: process.env.JWT_SIGNUP_SECRET_KEY,
+    jwtSignUpExpirationInMins: process.env.JWT_SIGNUP_EXPIRATION_IN_MINS,
   };
 }
 
