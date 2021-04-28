2843c341a73c09a1742bdeffc1128b3ce0de3195
dpns98 <dp99801@gmail.com>
2021-04-28 13:21:54 +0200

    added post request for adding new car

--- client/src/app/new-car/new-car.component.ts
+++ client/src/app/new-car/new-car.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { CarService } from '../services/car.service';
+import { Car } from '../models/car.model';
 
 @Component({
   selector: 'app-new-car',
@@ -12,12 +13,47 @@ export class NewCarComponent implements OnInit {
   public newCar: FormGroup;
   constructor(private carService: CarService, private formBuilder: FormBuilder) { 
     this.newCar = this.formBuilder.group({
-
+      userId: ['', []],
+      datePosted: ['', []],
+      type: ['', []],
+      make: ['', []],
+      model: ['', []],
+      year: ['', []],
+      mileage: ['', []],
+      engineSize: ['', []],
+      fuelType: ['', []],
+      emissionClass: ['', []],
+      horsepower: ['', []],
+      transmission: ['', []],
+      numberOfDoors: ['', []],
+      numberOfSeats: ['', []],
+      bootCapacity: ['', []],
+      AC: [false,[]],
+      body: ['', []],
+      color: ['', []],
+      damage: ['', []],
+      registeredUntil: ['', []],
+      country: ['', []],
+      price: ['', []],
+      images: this.formBuilder.array([this.formBuilder.control('')])
     });
   }
 
-  public add(car: object){
+  get images(){
+    return this.newCar.get('images') as FormArray;
+  }
 
+  addImage(){
+    this.images.push(this.formBuilder.control(''));
+  }
+
+  public add(car: any){
+    console.log(car)
+    console.log(car.images)
+    this.carService.postCar(car).
+      subscribe((c: Car) => {
+        window.alert(`Added car with id: ${c.id}`);
+      });
   };
 
   ngOnInit(): void {
