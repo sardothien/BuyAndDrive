f30de81f8d53c29a636b35e17da5e2ec14b8b48d
imafikus <aleksatesicteske@gmail.com>
2021-04-30 16:11:27 +0200

    Add red color for alerts on login page

--- client/tailwindcss/tailwind.config.js
+++ client/tailwindcss/tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: colors.blue,
+      green: colors.green,
+    },
     container: {
       center:true,
       padding: "2rem"
