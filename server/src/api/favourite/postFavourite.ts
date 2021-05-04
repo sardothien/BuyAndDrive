21a5f2d5f72dbb8122722ed8cd8a464edf50d847
Luka Vujcic <lukavujcic@hotmail.com>
2021-05-04 14:04:57 +0200

    Add handling if favourite already exists

--- server/src/api/favourite/postFavourite.ts
+++ server/src/api/favourite/postFavourite.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { insertFavourite } from './db'
+import { insertFavourite,isExistsInFavourite } from './db'
 import { getCarById, getUserById } from "../../db";
 import { sendResponse, InternalServerErrorResponse, InvalidReqContentResponse, InvalidReqStructureResponse, Statuses } from "../ApiResponse";
 export const postFavourite = async (req: Request, res: Response): Promise<void> => {
@@ -18,8 +18,17 @@ export const postFavourite = async (req: Request, res: Response): Promise<void> 
     if (!user){
       return sendResponse(res, InvalidReqContentResponse);
     }
-    await insertFavourite(carId, userId);
-    res.status(Statuses.ok).send({ msg: 'new favourite added: '});
+    const isExistsFav = await isExistsInFavourite(carId, userId)
+    if (isExistsFav)
+    {
+      res.status(Statuses.ok).send({msg:'favourite already exists'})  
+    }
+    else
+    {
+      await insertFavourite(carId, userId);
+      res.status(Statuses.ok).send({ msg: 'new favourite added: '});
+    }
+    
   }
   catch (err){
     return sendResponse(res, InternalServerErrorResponse);
