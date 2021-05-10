6995b3b4768a0bb7b0e4d959b9f1d8e3f2a0f952
imafikus <aleksatesicteske@gmail.com>
2021-05-10 10:35:56 +0200

    Add checks for proper paths

--- server/src/middlewares/tokenChecker.ts
+++ server/src/middlewares/tokenChecker.ts
@@ -13,49 +13,69 @@ const endpointsWithNoAuth: Record<string, Array<string>> = {
   '/submit_reset_password': ['POST',],
 }
 
+const endpointsWithAuth: Record<string, Array<string>> = {
+  '/new': ['POST',],
+  '/add_favourite': ['POST',],
+  '/remove_favourite': ['DELETE',],
+  '/filter_cars': ['GET',],
+}
+
 const adminEndpoints: Record<string, Array<string>> = {
-  '/approve_cars': ['POST', 'GET'],
+  '/approve_cars': ['GET'],
   '/reject_cars': ['POST',],
 }
 
-export const tokenChecker = async (req: Request, res: Response, next: NextFunction): void => {
+export const tokenChecker = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 
   const endpoint = req.path;
+  
+  if (!isValidEndpoint(endpoint)) {
+    res.status(Statuses.notFound).send('Invalid path');
+    return; 
+  }
 
   if (endpoint in endpointsWithNoAuth) {
 
-    if(methodIsAllowed(endpointsWithNoAuth, endpoint, req.method)) {
+    if (methodIsAllowed(endpointsWithNoAuth, endpoint, req.method)) {
       next();
       return;
-    } else { 
+    } else {
       res.status(Statuses.notAllowed).send('Method not allowed');
       return;
     }
   }
 
   const token = req.headers['authorization'];
-  const userId = tokens.verifyAccessToken(token);
-  
+  const userId = tokens.verifyAccessToken(token as string);
+
   if (userId) {
     const user = await db.getUserById(userId);
-    if(user?.isAdmin) {
-      if(methodIsAllowed(adminEndpoints, endpoint, req.method)) {
+    if (user?.isAdmin) {
+      if (methodIsAllowed(adminEndpoints, endpoint, req.method)) {
         next();
         return;
-      } else { 
-        res.status(Statuses.notAllowed).send('Method not allowed');
-        return;
       }
+    } else {
+      res.status(Statuses.notAllowed).send('Method not allowed');
+      return;
     }
-    
+
     res.locals.userId = userId;
     next();
     return;
   }
   res.status(Statuses.unauthorized).send('Invalid token.');
-  return;  
+  return;
 };
 
 const methodIsAllowed = (allEndpoints: Record<string, Array<string>>, specificEndpoint: string, method: string): boolean => {
   return allEndpoints[specificEndpoint].includes(method);
+}
+
+const isValidEndpoint = (endpoint: string): boolean => {
+  return (
+    endpoint in endpointsWithNoAuth ||
+    endpoint in endpointsWithAuth || 
+    endpoint in adminEndpoints
+  )
 }
