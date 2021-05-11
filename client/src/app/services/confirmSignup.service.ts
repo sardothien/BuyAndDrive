cdc41ccafb1a510de367016834cb8ad06e636255
imafikus <aleksatesicteske@gmail.com>
2021-05-11 17:10:27 +0200

    Add link_expired and signup_successful components

--- client/src/app/services/confirmSignup.service.ts
+++ client/src/app/services/confirmSignup.service.ts
@@ -2,14 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import {RegisterInfo} from '../models/register.model';
 import {Response} from '../models/register.response.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RegisterService {
+export class ConfirmSignupService {
 
-  private readonly url = `${environment.backendUrl}/signup`;
+  private readonly url = `${environment.backendUrl}/verify_signup`;
 
   constructor(private http: HttpClient) { }
 
