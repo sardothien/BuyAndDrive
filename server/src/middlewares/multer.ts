5bf1381722eea0130f94afbfc349397bde061e4c
Luka Vujcic <lukavujcic@hotmail.com>
2021-05-16 10:21:20 +0200

    Style: Added indentation in  fileFilter in Multer

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -2,12 +2,12 @@ import multer from 'multer';
 export const upload = multer({
   dest: 'uploads/',
   fileFilter: function (req: any, file: any, cb: any) {
-  const allowedTypes:string[]=['image/png','image/gif','image/jpeg','image/bmp']
-  if (allowedTypes.includes(file.mimetype)) 
-  {
-      return cb(null, true);
-  } else {
-      cb(null, false);
-  }
+    const allowedTypes:string[]=['image/png','image/gif','image/jpeg','image/bmp']
+    if (allowedTypes.includes(file.mimetype)) 
+    {
+        return cb(null, true);
+    } else {
+        cb(null, false);
+    }
   }
 });
