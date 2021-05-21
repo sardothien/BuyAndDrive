b7ace6717d7c047f33a338c978d9d4ed13fed3c9
dpns98 <dp99801@gmail.com>
2021-05-21 21:16:54 +0200

    images on favorites

--- client/src/app/favorites/favorites.component.ts
+++ client/src/app/favorites/favorites.component.ts
@@ -4,6 +4,7 @@ import { FavoritesService } from '../services/favorites.service';
 import { Observable } from 'rxjs';
 import { CarService } from '../services/car.service';
 import Swal from 'sweetalert2';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-favorites',
@@ -14,7 +15,10 @@ export class FavoritesComponent implements OnInit {
 
   public favorites!: Car[];
 
-  constructor(private favoritesService: FavoritesService, private carService: CarService) {
+  constructor(
+    private favoritesService: FavoritesService, 
+    private carService: CarService,
+    private sanitizer: DomSanitizer) {
     this.refresh();
   }
 
@@ -47,8 +51,17 @@ export class FavoritesComponent implements OnInit {
     for (let f of res.favourites){
       console.log(f);
       let c = await this.findById(f.carId);
+      c[0].images = await this.getImgsPath(c[0].id);
+      this.carService.getCarImage(c[0].images[0]).subscribe(data => {
+        let objectURL = URL.createObjectURL(data);
+        c[0].firstImage = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+      })
       this.favorites.push(c[0]);
     }
+  }
+
+  public getImgsPath(id): Promise<string[]>{
+    return this.carService.getCarImages(id).toPromise();
   }
 
   ngOnInit() {
