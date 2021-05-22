55f575df98d5eae0b8541ef59e5297a30033d168
Luka Vujcic <lukavujcic@hotmail.com>
2021-05-22 09:52:05 +0200

    Add .sql files for import

---
+++ server/data/_Images__202105212324.sql
@@ -0,0 +1,10 @@
+INSERT INTO public."Images" (id,"carId","imagePath") VALUES
+	 ('dccd9dc3-bb8e-4a6f-9c4b-6274f45a41f3'::uuid,'a082ad65-771b-4954-b169-2d4f12b5fe0c'::uuid,'uploads/9a66eb58d378371b5322defb9abf8d3a'),
+	 ('879c3e84-ba64-46f3-ba4c-c71b77c923bd'::uuid,'35f60f10-a9ac-4a04-8bd0-42b2240125d9'::uuid,'uploads/30a1c88e4925ba833211ecafa502b11e'),
+	 ('029c7e33-859a-4bc9-96cd-b3b80a5b3191'::uuid,'a082ad65-771b-4954-b169-2d4fe125fe0c'::uuid,'uploads/119131e12f2a782bdc97293b384c6273'),
+	 ('d6246c94-54b6-47d8-924e-f5bc0413a4e2'::uuid,'35f60f10-a9ac-4a04-8bd0-42b224058512'::uuid,'uploads/e7eab5e5ebba4ab06c40101f327ba5e1'),
+	 ('c4dc196a-70eb-4419-838a-19452e7cedc8'::uuid,'a9219d41-9862-4c4f-ab45-3103d1b8d8a3'::uuid,'uploads/e7e329bc5ed5f1852099a35161dedf44'),
+	 ('89759ebf-1ed0-40f7-9a54-68fd524b3f87'::uuid,'a082ad65-771b-4954-b169-2d4feeb5fe0c'::uuid,'uploads/2318125d55273b4995601570b288a7b5'),
+	 ('414c6011-6617-4425-99b3-0eafbf519fd7'::uuid,'35f60f10-a9ac-4a04-8bd0-42b2240585d9'::uuid,'uploads/fe6e42d14455bcd61e54fb99454b93ec'),
+	 ('2fb7ad75-349a-4fbf-a081-e5c023ed4103'::uuid,'a9219d41-9862-4c4f-ab45-3103d1b812a3'::uuid,'uploads/ce9fd34d52a83bf85378f4bb577de522'),
+	 ('aa8f2f7b-b2af-46e7-8b09-d345e0160416'::uuid,'a9219d41-9862-4c4f-ab45-3103d1b812a3'::uuid,'uploads/77e82c38c1a6f411ecf36c4cd4e582d2');
