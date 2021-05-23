472468b0280261893bb90e0bc48ea9126a9dcba2
AizenAngel <boza.mitrovic@gmail.com>
2021-05-23 23:20:21 +0200

    Add model that determins whether user is admin or not

---
+++ client/src/app/models/IsAdmin.model.ts
@@ -0,0 +1,4 @@
+export enum IsAdmin{
+    TRUE = "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
+    FALSE = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9"
+};
