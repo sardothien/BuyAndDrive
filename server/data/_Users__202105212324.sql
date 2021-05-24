d628eff6b7a6845660edabb94bd8a8cbe362b027
Luka Vujcic <lukavujcic@hotmail.com>
2021-05-24 11:18:28 +0200

    Change users data

--- server/data/_Users__202105212324.sql
+++ server/data/_Users__202105212324.sql
@@ -1,3 +1,3 @@
 INSERT INTO public."Users" (id,email,"firstName","lastName",verified,"isAdmin") VALUES
-	 ('b0310121-3a8b-4955-b257-609f11b4426b'::uuid,'neko@gmail.com','petar','petrovic',true,false),
-	 ('6e58856b-795e-40e2-93a1-be825e5af105'::uuid,'admin@gmail.com','admin','admin',true,false);
+	 ('b0310121-3a8b-4955-b257-609f11b4426b'::uuid,'petar@gmail.com','petar','petrovic',true,false),
+	 ('6e58856b-795e-40e2-93a1-be825e5af105'::uuid,'marko@gmail.com','marko','markovic',true,false);
