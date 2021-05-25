916f4695d05a2aeb21e11b2341a1704b1d3cdeec
Mina Milošević <mikimina98@gmail.com>
2021-05-25 09:24:10 +0200

    Add image carousel for cars on My Cars page

--- client/src/app/users-car/users-car.component.ts
+++ client/src/app/users-car/users-car.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Car, CarStatus } from '../models/car.model';
+import { CarService } from '../services/car.service';
 
 @Component({
   selector: 'app-users-car',
@@ -15,7 +17,11 @@ export class UsersCarComponent implements OnInit {
   public carStatus!: string;
   CarStatusEnum = CarStatus;
 
-  constructor() { }
+  public index: number = 0;
+  public image;
+
+  constructor(private carService: CarService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.setCarStatus();
@@ -31,6 +37,19 @@ export class UsersCarComponent implements OnInit {
     else {
       this.carStatus = this.CarStatusEnum.Sold;
     }
+  }
+
+  public getImg(path){
+    this.carService.getCarImage(path).subscribe(data => {
+      let objectURL = URL.createObjectURL(data);
+      this.image = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+    })
+  }
+
+  public nextImg(){
+    this.index = (this.index + 1) % this.car.images.length;
+    console.log(this.car.images.length);
+    this.getImg(this.car.images[this.index]);
   }
 
 }
